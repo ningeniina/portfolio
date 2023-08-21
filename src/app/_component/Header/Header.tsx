12be26c3eeac4ef8f6527dc4b86be5b4bf4c1543
Hito <tomonoshi001@gmail.com>
2023-08-22 08:06:20 +0900

    🚧 レイアウト調整

--- src/app/_component/Header/Header.tsx
+++ src/app/_component/Header/Header.tsx
@@ -37,7 +37,7 @@ export default function Header() {
               fill="#272343"
             />
           </svg>
-          ningeniina
+          portfolio
         </Link>
         <Menu />
       </div>
